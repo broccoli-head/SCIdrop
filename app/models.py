ed263b5df90ef4602b21373b1c85e071fcec9000
broccoli-head <luki.winners1@gmail.com>
2025-05-14 14:55:56 +0200

    database and media

--- app/models.py
+++ app/models.py
@@ -2,8 +2,11 @@ from django.db import models
 
 class Chest(models.Model):
     name = models.CharField(max_length = 100)
+    price = models.FloatField(default = 0)
+    cover = models.ImageField(upload_to='media/chestCovers', blank = True, null = True)
 
 class Skin(models.Model):
     chestID = models.ForeignKey(Chest, on_delete = models.CASCADE)
     name = models.CharField(max_length = 100)
-    price = models.FloatField()
+    price = models.FloatField(default = 0)
+    cover = models.ImageField(upload_to='media/skinCovers', blank = True, null = True)
