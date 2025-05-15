8744661b2a0a791193a19c79462b4cefda30b56b
broccoli-head <luki.winners1@gmail.com>
2025-05-15 17:30:17 +0200

    homepage styled and added  cover to the second chest

--- app/models.py
+++ app/models.py
@@ -1,13 +1,22 @@
 from django.db import models
 from decimal import Decimal
 
+rarityList = [
+    ('common', 'Common'),
+    ('rare', 'Rare'),
+    ('epic', 'Epic'),
+    ('legendary', 'Legendary'),
+]
+
 class Chest(models.Model):
     name = models.CharField(max_length = 100)
-    price = models.DecimalField(default = 0, max_digits = 10, decimal_places = 2)
+    price = models.DecimalField(default = 0, max_digits = 6, decimal_places = 2)
+    rarity = models.CharField(max_length = 20, choices = rarityList)
     cover = models.ImageField(upload_to='media/chestCovers', blank = True, null = True)
 
 class Skin(models.Model):
     chestID = models.ForeignKey(Chest, on_delete = models.CASCADE)
     name = models.CharField(max_length = 100)
-    price = models.DecimalField(default = 0, max_digits = 10, decimal_places = 2)
+    price = models.DecimalField(default = 0, max_digits = 6, decimal_places = 2)
+    rarity = models.CharField(max_length = 20, choices = rarityList)
     cover = models.ImageField(upload_to='media/skinCovers', blank = True, null = True)
