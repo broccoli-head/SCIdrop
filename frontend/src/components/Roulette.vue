e0e50522f0edb590ce89e09f9f7b9519cbcdd8eb
broccoli-head <luki.winners1@gmail.com>
2025-05-18 18:27:45 +0200

    spinning logic

--- frontend/src/components/Roulette.vue
+++ frontend/src/components/Roulette.vue
@@ -1,20 +1,24 @@
 <template>
-<div id="skinList">
-    <div v-for="skin in skins" :key="skin.id" class="skinBox">
-        <h2>{{ skin.name }}</h2>
-        <img :src="skin.cover" />
-        <p class="rarity" :class="skin.rarity">{{ skin.rarity }}</p>
-        <p class="price">{{ skin.price }} ZŁ</p>
+    <img class="triangle" src="@/assets/icons/triangle.svg" />
+    <div id="skinsOverlay">
+        <div id="skinList" ref="skinList">
+            <div v-for="skin in skins" :key="skin.id"class="skinBox">
+                <h2>{{ skin.name }}</h2>
+                <img :src="skin.cover" />
+                <p class="rarity" :class="skin.rarity">{{ skin.rarity }}</p>
+                <p class="price">{{ skin.price }} ZŁ</p>
+            </div>
+        </div>
     </div>
-</div>
-<button id="spin">Spin</button>
+    <button id="spin" @click="spin">Spin</button>
 </template>
 
 <script>
 export default {
     data() {
         return {
-            skins: []
+            skins: [],
+            isSpinning: false
         }
     },
     async created() {
@@ -22,20 +26,31 @@ export default {
         try {
             const response = await fetch(`http://localhost:8000/api/skins/${chestID}/`);
             const data = await response.json();
-            this.skins = this.shuffleArray(data);
+            
+            const shuffled = this.shuffleArray(data);
+            //fills list with 20x current array
+            this.skins = Array(20).fill(shuffled).flat();
+
         } catch (err) {
             console.error('Failed to fetch skins:', err);
         }
     },
+
     methods: {
         shuffleArray(array) {
             for(let i = array.length - 1; i > 0; i--) {
+                //randomize order of the array items
                 let j = Math.floor(Math.random() * (i + 1));
                 let temp = array[i];
                 array[i] = array[j];
                 array[j] = temp;
             }
             return array;
+        },
+        spin() {
+            const skinList = this.$refs.skinList;
+            //spins list from right to left
+            skinList.style.transform = "translateX(-2000%)";
         }
     }
 }
